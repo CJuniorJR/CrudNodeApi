25de43dcca1fffe1b0c9a33fd3d85b57ee6bd8a5
= <=>
2019-03-16 13:53:19 -0300

    Servidor com hapi criado

--- src/app.js
+++ src/app.js
@@ -1,8 +1,26 @@
-var http = require('http');
-var server = http.createServer(function(request,response){
+const hapi = require('hapi');
 
+const server = hapi.server({
+    host:'localhost',
+    port:8000
 });
 
-server.listen(3000, function(){
-    console.log("Servidor iniciado.");
+server.route({
+    method: 'GET',
+    path:'/user',
+    handler:function(request,response){
+        return user;
+    }
 });
+
+const start = async function(){
+    try {
+        await server.start();
+    } 
+    catch (err) {
+        console.log(err);
+        process.exit(1);
+    }
+    console.log('Servidor rodando em:',server.info.uri);
+};
+start();
