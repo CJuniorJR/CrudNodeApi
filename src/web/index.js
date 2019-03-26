8805e3240e6ff9665bcf7ce3176f79b6d5e3ccfb
= <=>
2019-03-26 16:15:30 -0300

    Removendo usuario

--- src/web/index.js
+++ src/web/index.js
@@ -1,12 +1,42 @@
 $(document).ready(function(){
     axios.get('http://localhost:8000/user').then(function(resposta){
         $(resposta.data).each(function(index, user){
-            $("#table").append('<tr><td>'+ user.nome +'</td>'+
-            '<td>'+ user.email +'</td>'+
-            '<td>'+ user.login +'</td>'+
-            '<td>'+ user.senha +'</td></tr>'); 
+            $("#table").append(usuario(user)); 
         });
     }).catch(error => {
         console.log("SHIT")
     });
-})
+});
+
+function usuario(user){
+    let linha = $("<tr>");
+    let tdNome = $("<td>").text(user.nome);
+    let tdEmail = $("<td>").text(user.email);
+    let tdLogin = $("<td>").text(user.login);
+    let tdSenha = $("<td>").text(user.senha);
+    let tdRemover = $("<td>");
+
+    let link = $("<a>").addClass("botao-remover").attr("href","#").text("Remover");
+    link.on("click",function(){
+        removerUsuario(user.id);
+    });
+
+
+    tdRemover.append(link);
+
+    linha.append(tdNome);
+    linha.append(tdEmail);
+    linha.append(tdLogin);
+    linha.append(tdSenha);
+    linha.append(tdRemover);
+
+    return linha;
+}
+
+function removerUsuario(id){
+    event.preventDefault();
+    $(this).hide();
+    axios.delete('http://localhost:8000/user/'+id).then(function(){      
+        console.log("Usuario com id "+ id + " Removido com sucesso");
+    });
+}
