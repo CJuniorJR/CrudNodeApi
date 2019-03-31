0993dd672cc1e2b29cce0bcbf20bdd2e49b2663d
Fabio Rodrigues <fabio.rodrigues018@gmail.com>
2019-03-31 15:42:34 -0300

    refactor criacao e exclusao de usuario

--- src/web/index.js
+++ src/web/index.js
@@ -1,106 +1,68 @@
-
-    ((container, $) => {
-
-        let controller = {
-            buscarUsuarios: () => {
-                axios.get('http://localhost:8000/user')
+((container, $) => {
+    let controller = {
+        buscarUsuarios: () => {
+            axios.get('http://localhost:8000/user')
                 .then(view.preencherListaUsuarios)
                 .catch(view.handleError);
+        },
+        salvarUsuario: () => {
+            let usuario = {
+                nome: $('#nome').val(),
+                email: $('#email').val(),
+                login: $('#login').val(),
+                senha: $('#senha').val()
             }
-        };
-
-        let view = {
-            preencherListaUsuarios: (resp) => {
-                $(resp.data).each((index, user) => {
-                    var linha = "<tr>"
-                    linha += "<td>" + user.nome + "</td>"
-                    linha += "<td>" + user.email + "</td>"
-                    linha += "<td>" + user.login + "</td>"
-                    linha += "<td><a class='user-item' href='#' id='" + user._id + "'>Excluir</a></td>"
-                    linha += "</tr>"
-                    $("#table-content").append(linha);
-                });
-            },
-            handleError: (err) => {
-                console.log(err)
-            }
-        }
-
-
-        let initHandlers = () => {
-            $(container).on('click','element',(e)=>{
-
+            axios.post('http://localhost:8000/user', usuario).then(function () {
+                controller.buscarUsuarios();
             })
+                .catch(view.handleError);
+        },
+        excluirUsuario: function (e) {
+            let id = $(this).attr('id');
+
+            axios.delete('http://localhost:8000/user/' + id)
+                .then(controller.buscarUsuarios)
+                .catch(view.handleError);
         }
+    };
 
-        let init = ()=>{
-            initHandlers();
-            controller.buscarUsuarios();
-        };
+    let view = {
+        preencherListaUsuarios: (resp) => {
+            $("#table-content").empty();
 
-        $(container).ready(() => {
-            init();
-        });
-    })(document, $);
+            if (resp.data.length == 0) {
+                var linha = "<tr><td colspan='4'>Não há registros</td></tr>"
+                $("#table-content").append(linha);
+                return;
+            }
 
+            $(resp.data).each((index, user) => {
+                var linha = "<tr>"
+                linha += "<td>" + user.nome + "</td>"
+                linha += "<td>" + user.email + "</td>"
+                linha += "<td>" + user.login + "</td>"
+                linha += "<td><a class='user-item' href='#' id='" + user._id + "'>Excluir</a></td>"
+                linha += "</tr>"
+                $("#table-content").append(linha);
+            });
+        },
+        handleError: (err) => {
+            console.log(err)
+        }
+    }
 
-$(document).ready(function () {
-    axios.get('http://localhost:8000/user').then(function (resposta) {
-        $(resposta.data).each(function (index, user) {
-            $("#table").append(usuario(user));
-        });
-    }).catch(error => {
-        console.log(error);
+    let initHandlers = () => {
+        $(container)
+            .on('click', '#enviar', controller.salvarUsuario)
+            .on('click', '.user-item', controller.excluirUsuario)
+    }
+
+    let init = () => {
+        initHandlers();
+        controller.buscarUsuarios();
+    };
+
+    $(container).ready(() => {
+        init();
     });
-
-    $('form').submit(novoUsuario);
-
-    function usuario(user) {
-        let linha = $("<tr>");
-        let tdNome = $("<td>").text(user.nome);
-        let tdEmail = $("<td>").text(user.email);
-        let tdLogin = $("<td>").text(user.login);
-        let tdSenha = $("<td>").text(user.senha);
-        let tdRemover = $("<td>");
-
-        let link = $("<a>").addClass("botao-remover").attr("href", "#").text("Remover");
-        link.on("click", function () {
-            removerUsuario(user._id);
-        });
-
-
-        tdRemover.append(link);
-
-        linha.append(tdNome);
-        linha.append(tdEmail);
-        linha.append(tdLogin);
-        linha.append(tdSenha);
-        linha.append(tdRemover);
-
-        return linha;
-    }
-
-    function novoUsuario() {
-        event.preventDefault();
-
-        let usuario = {
-            nome: $('#nome').val(),
-            email: $('#email').val(),
-            login: $('#login').val(),
-            senha: $('#senha').val()
-        }
-        axios.post('http://localhost:8000/user', usuario).then(function () {
-            console.log("cool");
-        });
-    }
-
-    function removerUsuario(id) {
-        event.preventDefault();
-        $(this).parent().parent().remove();
-        axios.delete('http://localhost:8000/user/' + id).then(function () {
-            console.log("Usuario com id " + id + " Removido com sucesso");
-        });
-    }
-});
-
-
+})(document, $);
